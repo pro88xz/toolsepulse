51661203eb179ef6d5e95af3a73e1389566461e7
pro88xz <mohamedbangurapro88@gmail.com>
2026-05-19 22:38:42 +0000

    Add Hilltopads site verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,6 +71,7 @@ export default function RootLayout({
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#2563eb" />
         <meta name="google-site-verification" content="6vkJFnHwHifL5TpqvVaG4vZo0dnsW1eJ65qSe0TF8zc" />
+        <meta name="b5be741c01525551266624e08456cf200e9b58ab" content="b5be741c01525551266624e08456cf200e9b58ab" />
           <meta name="google-adsense-account" content="ca-pub-9121922395304175" />
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9121922395304175" crossOrigin="anonymous"></script>
       </head>
